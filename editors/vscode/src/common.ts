f19f1289c0fa206d8de6d856d5900b800d5dd692
Luong Vo <vo.tran.thanh.luong@gmail.com>
2025-04-22 15:34:56 -0700

    init brand new world

---
+++ editors/vscode/src/common.ts
@@ -0,0 +1,262 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
+
+// cSpell: ignore codespaces
+
+// This file is common code shared by both vscode plugin entry points
+
+import * as vscode from "vscode";
+
+import * as wasm_preview from "./wasm_preview";
+import * as lsp_commands from "./lsp_commands";
+import * as snippets from "./snippets";
+
+import {
+    type BaseLanguageClient,
+    type LanguageClientOptions,
+    NotificationType,
+} from "vscode-languageclient";
+
+export class ClientHandle {
+    #client: BaseLanguageClient | null = null;
+    #updaters: ((c: BaseLanguageClient | null) => void)[] = [];
+
+    get client(): BaseLanguageClient | null {
+        return this.#client;
+    }
+
+    set client(c: BaseLanguageClient | null) {
+        this.#client = c;
+        for (const u of this.#updaters) {
+            u(this.#client);
+        }
+    }
+
+    public add_updater(u: (c: BaseLanguageClient | null) => void) {
+        u(this.#client);
+        this.#updaters.push(u);
+    }
+
+    async stop() {
+        const to_stop = this.client;
+        this.client = null;
+        for (const u of this.#updaters) {
+            u(this.#client);
+        }
+
+        if (to_stop) {
+            // mark as stopped so that we don't detect it as a crash
+            Object.defineProperty(to_stop, "slint_stopped", {
+                value: true,
+            });
+            await to_stop.stop();
+        }
+    }
+}
+
+const client = new ClientHandle();
+
+// LSP related:
+
+// Set up our middleware. It is used to redirect/forward to the WASM preview
+// as needed and makes the triggering side so much simpler!
+
+export function languageClientOptions(
+    schemes: string[],
+    telemetryLogger: vscode.TelemetryLogger,
+): LanguageClientOptions {
+    var document_selector = [];
+    for (var scheme of schemes) {
+        document_selector.push({ scheme: scheme, language: "slint" });
+        document_selector.push({ scheme: scheme, language: "rust" });
+    }
+
+    return {
+        documentSelector: document_selector,
+        middleware: {
+            async provideCodeActions(
+                document: vscode.TextDocument,
+                range: vscode.Range,
+                context: vscode.CodeActionContext,
+                token: vscode.CancellationToken,
+                next: any,
+            ) {
+                const actions = await next(document, range, context, token);
+                if (actions) {
+                    snippets.detectSnippetCodeActions(actions);
+                }
+                return actions;
+            },
+            window: {
+                async showDocument(params, next: any) {
+                    const cl = client.client;
+                    if (!params.external && cl) {
+                        // If the preview panel is open, the default behavior would be to open a document on the same column.
+                        // But we want to open the document next to it instead.
+                        const panel = wasm_preview.panel();
+                        if (panel && panel.active) {
+                            const uri = cl.protocol2CodeConverter.asUri(
+                                params.uri,
+                            );
+                            const col = panel.viewColumn || 1;
+                            const options: vscode.TextDocumentShowOptions = {
+                                viewColumn: col > 1 ? col - 1 : col + 1,
+                                preserveFocus: !params.takeFocus,
+                            };
+                            if (params.selection !== undefined) {
+                                options.selection =
+                                    cl.protocol2CodeConverter.asRange(
+                                        params.selection,
+                                    );
+                            }
+                            await vscode.window.showTextDocument(uri, options);
+
+                            return { success: true };
+                        }
+                    }
+                    return await next(params);
+                },
+            },
+            async provideCodeLenses(document, token, next) {
+                const lenses = await next(document, token);
+                if (lenses && lenses.length > 0) {
+                    maybeSendStartupTelemetryEvent(telemetryLogger);
+                }
+                return lenses;
+            },
+        },
+    };
+}
+
+// Setup code to be run *before* the client is started.
+// Use the ClientHandle for code that runs after the client is started.
+
+export function prepare_client(client: BaseLanguageClient) {
+    client.registerFeature(new snippets.SnippetTextEditFeature());
+}
+
+// VSCode Plugin lifecycle related:
+
+export function activate(
+    context: vscode.ExtensionContext,
+    startClient: (_client: ClientHandle, _ctx: vscode.ExtensionContext) => void,
+): vscode.StatusBarItem {
+    const statusBar = vscode.window.createStatusBarItem(
+        vscode.StatusBarAlignment.Left,
+    );
+
+    context.subscriptions.push(statusBar);
+    statusBar.text = "Slint";
+
+    client.add_updater((cl) => {
+        wasm_preview.initClientForPreview(context, cl);
+    });
+
+    vscode.workspace.onDidChangeConfiguration(async (ev) => {
+        if (ev.affectsConfiguration("slint")) {
+            client.client?.sendNotification(
+                "workspace/didChangeConfiguration",
+                { settings: "" },
+            );
+            wasm_preview.update_configuration();
+        }
+    });
+
+    startClient(client, context);
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand("slint.showPreview", async function () {
+            const ae = vscode.window.activeTextEditor;
+            if (!ae) {
+                return;
+            }
+
+            lsp_commands.showPreview(ae.document.uri.toString(), "");
+        }),
+    );
+
+    context.subscriptions.push(
+        vscode.commands.registerCommand("slint.reload", async function () {
+            statusBar.hide();
+            await client.stop();
+            startClient(client, context);
+        }),
+    );
+
+    vscode.window.registerWebviewPanelSerializer(
+        "slint-preview",
+        new wasm_preview.PreviewSerializer(context),
+    );
+
+    vscode.workspace.onDidChangeConfiguration(async (ev) => {
+        if (ev.affectsConfiguration("slint")) {
+            client.client?.sendNotification(
+                "workspace/didChangeConfiguration",
+                { settings: "" },
+            );
+        }
+    });
+
+    return statusBar;
+}
+
+export function deactivate(): Thenable<void> | undefined {
+    if (!client.client) {
+        return undefined;
+    }
+    return client.stop();
+}
+
+let telemetryEventSent = false;
+async function maybeSendStartupTelemetryEvent(
+    telemetryLogger: vscode.TelemetryLogger,
+) {
+    if (telemetryEventSent) {
+        return;
+    }
+    telemetryEventSent = true;
+
+    let usageData = {};
+
+    enum ProgrammingLanguage {
+        Rust = "Rust",
+        Cpp = "Cpp",
+        JavaScript = "JavaScript",
+        Python = "Python",
+    }
+
+    const projectLanguages = new Set<ProgrammingLanguage>();
+
+    if (vscode.workspace.workspaceFolders) {
+        const workspaceFolderContents = await Promise.all(
+            vscode.workspace.workspaceFolders.map((workspaceFolder) => {
+                return vscode.workspace.fs.readDirectory(workspaceFolder.uri);
+            }),
+        );
+
+        for (const path of workspaceFolderContents.flatMap((fileEntries) =>
+            fileEntries.map((fileEntry) => fileEntry[0].toLowerCase()),
+        )) {
+            if (path.endsWith("cargo.toml")) {
+                projectLanguages.add(ProgrammingLanguage.Rust);
+            } else if (path.endsWith("cmakelists.txt")) {
+                projectLanguages.add(ProgrammingLanguage.Cpp);
+            } else if (path.endsWith("package.json")) {
+                projectLanguages.add(ProgrammingLanguage.JavaScript);
+            } else if (
+                path.endsWith("pyproject.toml") ||
+                path.endsWith("requirements.txt")
+            ) {
+                projectLanguages.add(ProgrammingLanguage.Python);
+            }
+        }
+    }
+
+    if (projectLanguages.size > 0) {
+        usageData = Object.assign(usageData, {
+            projectLanguages: Array.from(projectLanguages.values()),
+        });
+    }
+
+    telemetryLogger.logUsage("extension-activated", usageData);
+}
---
+++ tests/screenshots/build.rs
@@ -0,0 +1,201 @@
+// Copyright © SixtyFPS GmbH <info@slint.dev>
+// SPDX-License-Identifier: GPL-3.0-only OR LicenseRef-Slint-Royalty-free-2.0 OR LicenseRef-Slint-Software-3.0
+
+use std::io::{BufWriter, Write};
+use std::path::Path;
+
+/// Returns a list of all the `.slint` files in the `tests/cases` subfolders.
+pub fn collect_test_cases() -> std::io::Result<Vec<test_driver_lib::TestCase>> {
+    let mut results = vec![];
+
+    let case_root_dir: std::path::PathBuf = [env!("CARGO_MANIFEST_DIR"), "cases"].iter().collect();
+
+    println!("cargo:rerun-if-env-changed=SLINT_TEST_FILTER");
+    let filter = std::env::var("SLINT_TEST_FILTER").ok();
+
+    for entry in walkdir::WalkDir::new(case_root_dir.clone()).follow_links(true) {
+        let entry = entry?;
+        let absolute_path = entry.into_path();
+        if absolute_path.is_dir() {
+            println!("cargo:rerun-if-changed={}", absolute_path.display());
+            continue;
+        }
+        let relative_path =
+            std::path::PathBuf::from(absolute_path.strip_prefix(&case_root_dir).unwrap());
+        if let Some(filter) = &filter {
+            if !relative_path.to_str().unwrap().contains(filter) {
+                continue;
+            }
+        }
+        if let Some(ext) = absolute_path.extension() {
+            if ext == "60" || ext == "slint" {
+                results.push(test_driver_lib::TestCase {
+                    absolute_path,
+                    relative_path,
+                    requested_style: None,
+                });
+            }
+        }
+    }
+    Ok(results)
+}
+
+fn main() -> std::io::Result<()> {
+    let default_font_path: std::path::PathBuf =
+        [env!("CARGO_MANIFEST_DIR"), "..", "..", "demos", "printerdemo", "ui", "fonts"]
+            .iter()
+            .collect();
+
+    std::env::set_var("SLINT_DEFAULT_FONT", default_font_path.clone());
+    println!("cargo:rustc-env=SLINT_DEFAULT_FONT={}", default_font_path.display());
+
+    let mut generated_file = BufWriter::new(std::fs::File::create(
+        Path::new(&std::env::var_os("OUT_DIR").unwrap()).join("generated.rs"),
+    )?);
+
+    let references_root_dir: std::path::PathBuf =
+        [env!("CARGO_MANIFEST_DIR"), "references"].iter().collect();
+
+    let font_cache = i_slint_compiler::FontCache::default();
+
+    for (i, testcase) in
+        test_driver_lib::collect_test_cases("screenshots/cases")?.into_iter().enumerate()
+    {
+        let mut reference_path = references_root_dir
+            .join(testcase.relative_path.clone())
+            .with_extension("png")
+            .to_str()
+            .unwrap()
+            .escape_default()
+            .to_string();
+
+        reference_path = format!("\"{reference_path}\"");
+
+        println!("cargo:rerun-if-changed={}", testcase.absolute_path.display());
+        let mut module_name = testcase.identifier();
+        if module_name.starts_with(|c: char| !c.is_ascii_alphabetic()) {
+            module_name.insert(0, '_');
+        }
+        writeln!(generated_file, "#[path=\"{module_name}.rs\"] mod r#{module_name};")?;
+        let source = std::fs::read_to_string(&testcase.absolute_path)?;
+
+        let needle = "SLINT_SCALE_FACTOR=";
+        let scale_factor = source.find(needle).map(|p| {
+            let source = &source[p + needle.len()..];
+            let scale_factor: f32 = source
+                .find(char::is_whitespace)
+                .and_then(|end| source[..end].parse().ok())
+                .unwrap_or_else(|| {
+                    panic!("Cannot parse {needle} for {}", testcase.relative_path.display())
+                });
+            scale_factor
+        });
+
+        let needle = "ROTATION_THRESHOLD=";
+        let rotation_threshold = source.find(needle).map_or(0., |p| {
+            source[p + needle.len()..]
+                .find(char::is_whitespace)
+                .and_then(|end| source[p + needle.len()..][..end].parse().ok())
+                .unwrap_or_else(|| {
+                    panic!("Cannot parse {needle} for {}", testcase.relative_path.display())
+                })
+        });
+        let skip_clipping = source.contains("SKIP_CLIPPING");
+
+        let needle = "SIZE=";
+        let (size_w, size_h) = source.find(needle).map_or((64, 64), |p| {
+            source[p + needle.len()..]
+                .find(char::is_whitespace)
+                .and_then(|end| source[p + needle.len()..][..end].split_once('x'))
+                .and_then(|(w, h)| Some((w.parse().ok()?, h.parse().ok()?)))
+                .unwrap_or_else(|| {
+                    panic!("Cannot parse {needle} for {}", testcase.relative_path.display())
+                })
+        });
+
+        let mut output = BufWriter::new(std::fs::File::create(
+            Path::new(&std::env::var_os("OUT_DIR").unwrap()).join(format!("{module_name}.rs")),
+        )?);
+
+        generate_source(
+            source.as_str(),
+            &mut output,
+            testcase,
+            scale_factor.unwrap_or(1.),
+            &font_cache,
+        )
+        .unwrap();
+
+        write!(
+            output,
+            r"
+    #[test] fn t_{i}() -> Result<(), Box<dyn std::error::Error>> {{
+    use crate::testing;
+
+    let window = testing::init_swr();
+    window.set_size(slint::PhysicalSize::new({size_w}, {size_h}));
+    let screenshot = {reference_path};
+    let options = testing::TestCaseOptions {{ rotation_threshold: {rotation_threshold}f32, skip_clipping: {skip_clipping} }};
+
+    let instance = TestCase::new().unwrap();
+    instance.show().unwrap();
+
+    testing::assert_with_render(screenshot, window.clone(), &options);
+
+    testing::assert_with_render_by_line(screenshot, window.clone(), &options);
+
+    Ok(())
+    }}",
+        )?;
+    }
+
+    //Make sure to use a consistent style
+    println!("cargo:rustc-env=SLINT_STYLE=fluent");
+    println!("cargo:rustc-env=SLINT_ENABLE_EXPERIMENTAL_FEATURES=1");
+
+    Ok(())
+}
+
+fn generate_source(
+    source: &str,
+    output: &mut impl Write,
+    testcase: test_driver_lib::TestCase,
+    scale_factor: f32,
+    font_cache: &i_slint_compiler::FontCache,
+) -> Result<(), std::io::Error> {
+    use i_slint_compiler::{diagnostics::BuildDiagnostics, *};
+
+    let include_paths = test_driver_lib::extract_include_paths(source)
+        .map(std::path::PathBuf::from)
+        .collect::<Vec<_>>();
+
+    let mut diag = BuildDiagnostics::default();
+    let syntax_node = parser::parse(source.to_owned(), Some(&testcase.absolute_path), &mut diag);
+    let mut compiler_config = CompilerConfiguration::new(generator::OutputFormat::Rust);
+    compiler_config.include_paths = include_paths;
+    compiler_config.embed_resources = EmbedResourcesKind::EmbedTextures;
+    compiler_config.enable_experimental = true;
+    compiler_config.style = Some("fluent".to_string());
+    compiler_config.const_scale_factor = scale_factor.into();
+    compiler_config.font_cache = font_cache.clone();
+    let (root_component, diag, loader) =
+        spin_on::spin_on(compile_syntax_node(syntax_node, diag, compiler_config));
+
+    if diag.has_errors() {
+        diag.print_warnings_and_exit_on_error();
+        return Err(std::io::Error::new(
+            std::io::ErrorKind::Other,
+            format!("build error in {:?}", testcase.absolute_path),
+        ));
+    } else {
+        diag.print();
+    }
+
+    generator::generate(
+        generator::OutputFormat::Rust,
+        output,
+        &root_component,
+        &loader.compiler_config,
+    )?;
+    Ok(())
+}
